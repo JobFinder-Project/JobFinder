4008a96d3e8bd1f2df37984502bf47a0afb39809
Felipe William <felipe.william.silva@outlook.com>
2025-10-08 19:34:46 -0400

    :lock: fix: removendo o db local e adicionando o online com dotenv #139

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
 const mongoose =require('mongoose')
 
+
 // Configuração do Banco de Dados
-const dbUri = 'mongodb://localhost:27017/JobFinder'
+//const dbUri = 'mongodb://localhost:27017/JobFinder'
+const dbUri = process.env.MONGO_URI
+
+
 module.exports = () => mongoose.connect(dbUri)
