41794a7447b6e5b029a5f7ac700fc5a261df66a6
Felipe William <felipe.william.silva@outlook.com>
2026-02-05 11:43:48 -0400

    :sparkles: feat: cria componentes de layout reutilizáveis #162
    - Cria PageLayout para estrutura base (header, main, footer)
    - Cria DashboardLayout para dashboards de candidato
    - Cria AuthLayout para páginas de autenticação
    - Layouts prontos para uso progressivo

---
+++ frontend/src/components/Layout/index.js
@@ -0,0 +1,3 @@
+export { default as PageLayout } from './PageLayout/PageLayout'
+export { default as DashboardLayout } from './DashboardLayout/DashboardLayout'
+export { default as AuthLayout } from './AuthLayout/AuthLayout'
