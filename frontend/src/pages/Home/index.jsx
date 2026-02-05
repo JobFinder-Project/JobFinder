5afe11b02199077fee33f2422f66e82f435bb236
Felipe William <felipe.william.silva@outlook.com>
2026-02-04 20:57:44 -0400

    :tada: init: configura projeto React com Vite #162

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css'
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>JobFinder - Em construção</h1>
+      <p>Migração do frontend em andamento...</p>
+    </div>
+  )
+}
+
+export default Home
