2732158cd02c2072a5530cba6e676152a29eb5b2
Felipe Silva <felipewilliam.gs@gmail.com>
2026-08-26 09:56:57 -0400

    :package: build: adicionando dependências e configurações dos testes na aplicação #176

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,20 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        lines: 35,
+        functions: 30,
+        branches: 45,
+        statements: 35
+      }
+    }
   }
 })
